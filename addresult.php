22d314232b7468a04ca705344a3a730b62f32ed0
karim <karimxhaban@gmail.com>
2023-12-06 06:48:16 -0500

    1.2.2

---
+++ addresult.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="style.css">
+
+<i>stuednt ID:</i>
+<input type="number" name="stdId" id="">
+
+<i>Exam ID:</i>
+<input type="number" name="examId" id="">
+
+<i>Year:</i>
+<input type="number" name="stdId" id="">
+
+<i>Kiswahili:</i>
+<input type="number" name="kiswahili" id="">
+
+<i>Sayansi:</i>
+<input type="number" name="sayansi" id="">
+
+<i>Hisabati:</i>
+<input type="number" name="hisabati" id="">
+
+<i>M/J:</i>
+<input type="number" name="m/j" id="">
+
+<i>U/M:</i>
+<input type="number" name="u/m" id="">
+
+<i>English:</i>
+<input type="number" name="english" id="">
